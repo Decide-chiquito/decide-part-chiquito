52b9878976741a868c2a9461058e901bd41fe115
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2023-11-13 11:43:43 +0100

    feat: Mejora del filtro por tags en "Census" #58
    Mejora del filtro por tags incluyendo un buscador por nombre para buscar y seleccionar etiquetas de manera más eficiente

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
 from .models import Tag, Census
+from admin_auto_filters.filters import AutocompleteFilter
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    search_fields = ('name', )
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class TagAutocompleteFilter(AutocompleteFilter):
+    title = 'Tag'  # El título que se mostrará para el filtro.
+    field_name = 'tags'  # El nombre del campo ManyToMany en el modelo Census.
 
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id', 'adscription_center', 'display_tags')
-    list_filter = ('voting_id', 'adscription_center', 'tags')
+    list_filter = ('voting_id', 'adscription_center', TagAutocompleteFilter)
     search_fields = ('voter_id', 'adscription_center')
+    autocomplete_fields = ['tags']
 
     def display_tags(self, obj):
         return ", ".join([tag.name for tag in obj.tags.all()])
--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'admin_auto_filters',
     'corsheaders',
     'django_filters',
     'rest_framework',
