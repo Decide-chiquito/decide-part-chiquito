1e0865e2ae2889f99cb475b971d9d751ceb1f9ee
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2023-11-13 10:53:27 +0100

    Feat: Creado sistema de etiquetado para organizar los censos #58
    Creación del modelo "Tag", asociado a "Census" para poder organizar los censos y filtrar por etiqueta

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Census
+from .models import Tag, Census
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
 
 class CensusAdmin(admin.ModelAdmin):
-    list_display = ('voting_id', 'voter_id', 'adscription_center')
-    list_filter = ('voting_id', 'adscription_center')
+    list_display = ('voting_id', 'voter_id', 'adscription_center', 'display_tags')
+    list_filter = ('voting_id', 'adscription_center', 'tags')
     search_fields = ('voter_id', 'adscription_center')
 
+    def display_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    display_tags.short_description = 'Tags'
 
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Census, CensusAdmin)
