7858cecbbcaf830ad0b7257ff6abcca24af7cfe7
alesanfe <alex.0002002@gmail.com>
2023-11-14 13:21:38 +0100

    feat Cambio de contraseña
    Se ha implementado la funcionalidad entera de manera manual, sin hacer uso de la que venía por defecto con Django debido a que no manda el correo

--- decide/users/views.py
+++ decide/users/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, redirect
+import re
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMultiAlternatives
+from django.db import IntegrityError
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import get_template
+from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from django.db import IntegrityError
-from django.contrib.auth import authenticate, login, logout
+from users.forms import EmailForm, PasswordForm
+
+from decide import settings
 
 
 class RegisterView(APIView):
@@ -27,7 +39,6 @@ class RegisterView(APIView):
         try:
             user = User.objects.create_user(username, password=password, email=email)
             token, _ = Token.objects.get_or_create(user=user)
-            success_message = 'Registro exitoso. Ahora estás registrado.'
             return Response({'user_pk': user.pk, 'token': token.key}, status=status.HTTP_201_CREATED)
         except IntegrityError:
             return Response({'error': 'El nombre de usuario ya está en uso.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -57,3 +68,80 @@ class LogoutView(APIView):
     def post(self, request):
         logout(request)
         return redirect('/')
+
+class RequestPasswordReset(APIView):
+    def post(self, request, *args, **kwargs):
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            user = get_object_or_404(User, email=form.cleaned_data['email'])
+
+            if self.validate_email(user.email):
+                # Generar el token único
+                token = default_token_generator.make_token(user)
+
+                # Generar la URL de verificación por correo electrónico
+                uid = urlsafe_base64_encode(force_bytes(user.pk))
+                verify_url = reverse('users:change_password', args=[uid, token])
+
+                # Enviar el correo electrónico de verificación
+                template = get_template('registration/password_email.html')
+                content = template.render({'new_password_url': settings.BASEURL + verify_url, 'username': user.username})
+                message = EmailMultiAlternatives(
+                    'Cambio de contraseña',
+                    content,
+                    settings.EMAIL_HOST_USER,
+                    [user.email]
+                )
+
+                message.attach_alternative(content, 'text/html')
+                message.send()
+                return redirect('/')
+
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        form = EmailForm()
+        return render(request, 'registration/make_petition_form.html', {'form': form})
+
+    def validate_email(self, email):
+        patron = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        if re.match(patron, email):
+            return True
+        else:
+            return False
+
+
+
+class ChangePassword(APIView):
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+        if user is not None and default_token_generator.check_token(user, token):
+            form = PasswordForm()
+            return render(request, 'registration/change_password_form.html', {'form': form})
+        else:
+            return render(request, 'registration/petition_new_password_error.html')
+
+    def post(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+        if user is not None and default_token_generator.check_token(user, token):
+            form = PasswordForm(request.POST)
+            form.old_password = user.password
+            if form.is_valid():
+                user.password = make_password(form.cleaned_data['password'])
+                user.save()
+                print("Password changed")
+                print(form.cleaned_data['password'])
+                return redirect('/')
+            else:
+                return render(request, 'registration/change_password_form.html', {'form': form})
+        else:
+            return render(request, 'registration/petition_new_password_error.html')
+
+    def get_user(self, uidb64):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+            print(user)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        return user
