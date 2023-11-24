b9b441ed76e4d1662beab88fcf1c8480b9e936ee
alepervaz <alepervaz@alum.us.es>
2023-11-24 17:32:51 +0100

    test: test listVisualizer

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -2,7 +2,14 @@ from django.test import TestCase
 from base.tests import BaseTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
+from rest_framework.test import APIClient
 from voting.models import Voting, Question
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from census.models import Census
+from voting.models import Voting
+from django.utils import timezone
+from datetime import timedelta, datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -33,3 +40,37 @@ class VisualizerTestCase(StaticLiveServerTestCase):
         response =self.driver.get(f'{self.live_server_url}/visualizer/{v.pk}/')
         vState= self.driver.find_element(By.TAG_NAME,"h2").text
         self.assertTrue(vState, "Votación no comenzada")
+
+class ListVisualizerTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.list_visualizer_url = reverse_lazy('listEnd') 
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(self.list_visualizer_url)
+        self.assertRedirects(response, '/users/login',status_code=302,target_status_code=301)
+
+    def test_logged_in_no_census(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.list_visualizer_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'visualizer/listVisualizer.html')
+        self.assertEqual(len(response.context['visualizers']), 0)
+
+    def test_logged_in_with_census_no_voting(self):
+        Census.objects.create(voter_id=self.user.id, voting_id=1)  # Asegúrate de que este ID exista o ajusta según tu modelo
+        self.client.force_login(self.user)
+        response = self.client.get(self.list_visualizer_url)
+        self.assertEqual(len(response.context['visualizers']), 0)
+
+    def test_logged_in_with_census_and_voting(self):
+        q = Question(desc='test question1')
+        q.save()
+        voting = Voting.objects.create(id=1,name="vting1",desc="desc1",question=q,start_date=timezone.now(), end_date=datetime.now() - timedelta(days=1),
+                                       type='IDENTITY',seats=10)
+        Census.objects.create(voter_id=self.user.id, voting_id=voting.id)
+        self.client.force_login(self.user)
+        response = self.client.get(self.list_visualizer_url)
+        self.assertEqual(len(response.context['visualizers']), 1)
+
