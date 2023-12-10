832df8edba9591dd01fcefa0008316cd5992b55b
alepervaz <alepervaz@alum.us.es>
2023-12-10 23:30:44 +0100

    fix listBooth,add attrributes list_display admin

--- decide/voting/admin.py
+++ decide/voting/admin.py
@@ -127,7 +127,7 @@ class QuestionAdmin(ModelAdmin):
 
 @admin.register(Voting)
 class VotingAdmin(ModelAdmin):
-    list_display = ('name', 'start_date', 'end_date','method','seats')
+    list_display = ('id','name', 'start_date', 'end_date','method','seats')
 
     readonly_fields = ('start_date', 'end_date', 'pub_key',
                        'tally', 'postproc')
