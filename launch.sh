d6a807f0712217456eceef19ac01d4f98ba5d32e
“sergiosant02” <sergiosantiago0403@gmail.com>
2023-12-14 21:09:23 +0100

    fix launch.sh not launch

--- launch.sh
+++ launch.sh
@@ -6,4 +6,4 @@ cp local_settings.deploy.py local_settings.py
 ./manage.py collectstatic --noinput
 ./manage.py makemigrations
 ./manage.py migrate
-gunicorn -w 5 decide.wsgi:application --timeout=500
+#gunicorn -w 5 decide.wsgi:application --timeout=500
