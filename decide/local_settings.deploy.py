d9326919570f53ff7a23425280e6e295a3a16de3
“sergiosant02” <sergiosantiago0403@gmail.com>
2023-12-14 23:04:54 +0100

    feat autodeploy

--- decide/local_settings.deploy.py
+++ decide/local_settings.deploy.py
@@ -61,8 +61,9 @@ MIDDLEWARE = [
 
 STATIC_URL='/static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
