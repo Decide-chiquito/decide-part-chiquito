6438aa8fb5aabaadaf7653d73657917e0243c129
carloosbaquero <carlos.baqueville@gmail.com>
2023-12-14 21:41:15 +0100

    test show live statictics static
    Test para comprabar que la función live_tally funciona correctamente

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -130,13 +130,6 @@ class LiveStaticticsSeleniumTests(StaticLiveServerTestCase):
         super().tearDown()
         self.driver.quit()
         self.base.tearDown()
-
-    def encrypt_msg(self, msg, v, bits=settings.KEYBITS):
-        pk = v.pub_key
-        p, g, y = (pk.p, pk.g, pk.y)
-        k = MixCrypt(bits=bits)
-        k.k = ElGamal.construct((p, g, y))
-        return k.encrypt(msg)
     
     def create_voting(self):
         q = Question(desc='test question')
@@ -153,48 +146,8 @@ class LiveStaticticsSeleniumTests(StaticLiveServerTestCase):
         v.auths.add(a)
 
         return v
-    
-    def create_voters(self, v):
-        for i in range(10):
-            u, _ = User.objects.get_or_create(username='testvoter{}'.format(i))
-            u.is_active = True
-            u.save()
 
-            c = Census(voter_id=u.id, voting_id=v.id, adscription_center='ETSII', )           
-            c.save()
-    
-    def get_or_create_user(self, pk):
-        user, _ = User.objects.get_or_create(pk=pk)
-        user.username = 'user{}'.format(pk)
-        user.set_password('qwerty')
-        user.save()
-        return user
-    
-    def store_votes(self, v):
-        voters = list(Census.objects.filter(voting_id=v.id))
-
-        voter = voters.pop()
-
-        clear = {}
-        for opt in v.question.options.all():
-            clear[opt.number] = 0
-            for i in range(random.randint(0, 5)):
-                a, b = self.encrypt_msg(opt.number, v)
-                data = {
-                    'voting': v.id,
-                    'voter': voter.voter_id,
-                    'vote': { 'a': a, 'b': b },
-                }
-                clear[opt.number] += 1
-                user = self.get_or_create_user(voter.voter_id)
-                self.base.login(user=user.username)
-                if len(voters) == 0:
-                    break
-                voter = voters.pop()
-                mods.post('store', json=data)
-        return clear
-
-    def test_resultados(self):
+    def test_liveStatictics(self):
 
         voting1 = self.create_voting()
         
@@ -212,7 +165,7 @@ class LiveStaticticsSeleniumTests(StaticLiveServerTestCase):
         self.driver.get(self.live_server_url + "/admin/voting/voting/")
         self.driver.set_window_size(1920, 1080)
 
-        self.driver.find_element(By.ID, "id_username").send_keys("admin")
+        self.driver.find_element(By.ID, "id_username").send_keys("admintest")
         self.driver.find_element(By.ID, "id_password").send_keys("qwerty")
         self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
 
@@ -250,7 +203,91 @@ class LiveStaticticsSeleniumTests(StaticLiveServerTestCase):
         elements = self.driver.find_elements(By.CLASS_NAME, "title")
         assert len(elements) > 0
 
+
+class LiveStaticticsBaseTests(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def encrypt_msg(self, msg, v, bits=settings.KEYBITS):
+        pk = v.pub_key
+        p, g, y = (pk.p, pk.g, pk.y)
+        k = MixCrypt(bits=bits)
+        k.k = ElGamal.construct((p, g, y))
+        return k.encrypt(msg)
+
+    def create_voting(self):
+        q = Question(desc='test question')
+        q.save()
+        for i in range(5):
+            opt = QuestionOption(question=q, option='option {}'.format(i+1))
+            opt.save()
+        v = Voting(name='test voting', question=q)
+        v.save()
+
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+                                          defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        v.auths.add(a)
+
+        return v
+
+    def create_voters(self, v):
+        for i in range(100):
+            u, _ = User.objects.get_or_create(username='testvoter{}'.format(i))
+            u.is_active = True
+            u.save()
+            c = Census(voter_id=u.id, voting_id=v.id)
+            c.save()
+
+    def get_or_create_user(self, pk):
+        user, _ = User.objects.get_or_create(pk=pk)
+        user.username = 'user{}'.format(pk)
+        user.set_password('qwerty')
+        user.save()
+        return user
+
+    def store_votes(self, v):
+        voters = list(Census.objects.filter(voting_id=v.id))
+        voter = voters.pop()
+
+        clear = {}
+        for opt in v.question.options.all():
+            clear[opt.number] = 0
+            for i in range(random.randint(0, 5)):
+                a, b = self.encrypt_msg(opt.number, v)
+                data = {
+                    'voting': v.id,
+                    'voter': voter.voter_id,
+                    'vote': { 'a': a, 'b': b },
+                }
+                clear[opt.number] += 1
+                user = self.get_or_create_user(voter.voter_id)
+                self.login(user=user.username)
+                voter = voters.pop()
+                mods.post('store', json=data)
+        return clear
+    
+    def test_liveStatictics(self):
         
+        v = self.create_voting()
+        self.create_voters(v)
+
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+
+        clear = self.store_votes(v)
+
+        self.login(user='admin')
+
+        live_tally = v.live_tally(self.token)
+        v.tally_votes(self.token)
+        tally = v.postproc
+
+        self.assertEqual(tally, live_tally)
 
 class ListVisualizerTests(TestCase):
     def setUp(self):
