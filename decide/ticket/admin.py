8c97c4035f38218804ada206dfffb60e82416632
CarmenRuizPorcel <carruipor@alum.us.es>
2023-12-16 12:18:55 +0100

    Feat: Complete Ticket app #145 Addition of ticket to admin and creation of the buttom to report an incidence to create a new ticket, the admin can set the ticket status to solve or rejected (in case it cant be solve or they reject the sugestion). This functionality is completely functional now.

--- decide/ticket/admin.py
+++ decide/ticket/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from .models import Ticket, TicketStatus
+from .filters import StatusFilter
+
+
+
+def solve_ticket(ModelAdmin, request, queryset):
+    for t in queryset.all():
+        t.status = 'SOLVED'
+        t.save()
+
+def reject_ticket(ModelAdmin, resquest, queryset):
+    queryset.update(status='REJECTED')
+
+
+@admin.register(Ticket)
+class TicketAdmin(ModelAdmin):
+    list_display = ('title', 'description', 'status')
+
+    list_filter = (StatusFilter,)
+    search_fields = ('title','status')
+
+    actions = [solve_ticket, reject_ticket]
+
+
+
+
--- decide/ticket/models.py
+++ decide/ticket/models.py
@@ -6,13 +6,20 @@ from ticket.enums import TicketStatus
 class Ticket(models.Model):
     title = models.CharField(max_length=64, blank=False, null=False)
     description = models.CharField(max_length=1024, blank=False, null=False)
-    status = models.CharField(max_length=16, choices=TicketStatus.choices(), default=TicketStatus.PENDING, blank=False, null=False)
+
+    STATUS = (
+        ('PENDING', 'Pending'),
+        ('SOLVED', 'Solved'),
+        ('REJECTED', 'Rejected'),
+    )
+    status = models.CharField(max_length=16, choices=STATUS, default='PENDING', blank=False, null=False, verbose_name="status")
+
+    
 
     class Meta:
         verbose_name = "Ticket"
         verbose_name_plural = "Tickets"
-        indexes = [
-        ]
+        
 
     def __str__(self):
         return f"{self.title} : {self.description} -> {self.status}"
