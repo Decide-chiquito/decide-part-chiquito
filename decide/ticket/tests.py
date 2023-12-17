20f1ddbdfa6920c25edf0a49ba9a8aaa40fe34d5
CarmenRuizPorcel <carruipor@alum.us.es>
2023-12-17 12:50:13 +0100

    Test: View test #145
    Test with selenium to test the view of creation of a new ticket

--- decide/ticket/tests.py
+++ decide/ticket/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase, Client
+from base.tests import BaseTestCase
 from django.urls import reverse
 from .models import Ticket
 from .admin import TicketAdmin
 from django.contrib.admin.sites import AdminSite
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from ticket import admin
 
@@ -43,3 +54,47 @@ class TicketAdminTestCase(TestCase):
         self.admin_site.reject_ticket(None, Ticket.objects.filter(id=self.ticket.id))
         self.ticket.refresh_from_db()
         self.assertEqual(self.ticket.status, 'REJECTED')
+
+class TestReportIncidence(StaticLiveServerTestCase):
+  def setUp(self):
+    self.base = BaseTestCase()
+    self.base.setUp()
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    self.driver = webdriver.Chrome(options=options)
+    super().setUp()
+  
+  def tearDown(self):
+    super().tearDown()
+    self.driver.quit()
+    self.base.tearDown()
+  
+  def test_reportIncidence(self):
+    home = self.live_server_url + "/"
+    self.driver.get(self.live_server_url)
+    self.driver.set_window_size(1480, 808)
+    self.driver.find_element(By.CSS_SELECTOR, ".button-ticket").click()
+
+    self.assertTrue(self.driver.current_url == f'{self.live_server_url}/ticket/add-ticket/')
+
+    self.driver.find_element(By.ID, "id_title").click()
+    self.driver.find_element(By.ID, "id_title").send_keys("Ticket de prueba")
+    self.driver.find_element(By.ID, "id_description").click()
+    self.driver.find_element(By.ID, "id_description").send_keys("Esto es una prueba")
+    self.driver.find_element(By.CSS_SELECTOR, ".filled-button:nth-child(2)").click()
+
+    time.sleep(2)
+
+    title = self.driver.find_element(By.ID, "id_title").text
+    description = self.driver.find_element(By.ID, "id_description").text
+
+    self.assertEqual(title, '')
+    self.assertEqual(description, '')
+
+    self.driver.find_element(By.ID, "return").click()
+
+    time.sleep(2)
+
+    self.assertEqual(self.driver.current_url, home)
+
+  
